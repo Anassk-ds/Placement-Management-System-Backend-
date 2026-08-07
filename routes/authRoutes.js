49cbad93a1c0518cb9f752ee35e6c71651b50155
Anas Shaik <anasshaik382@gmail.com>
2026-08-07 22:43:48 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,16 @@
+import express from "express";
+import { login, getMe, changePassword } from "../controllers/authControllers.js";
+import { requireAuth } from "../middleware/auth.js";
 
+const router = express.Router();
+
+// POST login
+router.post("/login", login);
+
+// GET current logged-in user
+router.get("/me", requireAuth, getMe);
+
+// PUT change password
+router.put("/change-password", requireAuth, changePassword);
+
+export default router;
